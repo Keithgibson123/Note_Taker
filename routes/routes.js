8e3f2cacdb1e95cde32ee4f73e1faa09e98f2d7f
unknown <keithgibson58@gmail.com>
2023-03-28 16:55:50 -0600

    added routes with notes

---
+++ routes/routes.js
@@ -0,0 +1,39 @@
+// dependencies 
+const path = require('path');
+const fs = require('fs')
+var uniqid = require('uniqid');
+
+
+// routing
+module.exports = (app) => {
+
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  // This is the post /api/notes 
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    //This is making a body
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      //calling for the unique id
+      id: uniqid(),
+    };
+    // This is making a push file for the db
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
+
+  // This is the delete
+  app.delete('/api/notes/:id', (req, res) => {
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+    
+  })
+};
